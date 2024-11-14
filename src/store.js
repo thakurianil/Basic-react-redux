15f02f3de8494632ed2cccb0623243c35a4756ea
Anil khand <anilkhandthakuri@gmail.com>
2024-11-14 20:28:08 +1100

    Update redux

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import {configureStore} from "@reduxjs/toolkit";
+
+const store = configureStore({
+    reducer:{
+        userData: {},
+
+    }
+})
+
+export default store;
---
+++ src/userSlice.js
@@ -0,0 +1,21 @@
+// import{createSlice} from "@reduxjs/toolkit";
+
+// const initialState = {
+//     userList: ['asd','asd']
+// }
+
+// const userSlice = createSlice({
+//     name: "user",
+//     initialState,
+//     reducers : {
+//         setUserList : (state, action) = >{
+//             state.userList = [..state.userList,
+//             action.payload];
+//         }
+//     }
+
+// })
+
+// const { reducer, actions } = userSlice
+
+// export default reducer;
